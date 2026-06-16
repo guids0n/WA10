279d0e6d8d260ae8f13327ea8efe96ce9c4d95a9
guids0n <guidsonbarreto@gmail.com>
2026-06-16 19:50:05 -0300

    Remove CrossOrigin annotation from DocumentoController
    Removed CrossOrigin annotation to allow for default CORS settings.

--- WA10-main/backendWA10/src/main/java/com/wa10/api/controller/DocumentoController.java
+++ WA10-main/backendWA10/src/main/java/com/wa10/api/controller/DocumentoController.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/documentos")
-@CrossOrigin(origins = "http://localhost:3000")
 public class DocumentoController {
 
     @Autowired
