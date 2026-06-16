2fe6f1369987f04a1ed7e8022991c3858b92032d
guids0n <guidsonbarreto@gmail.com>
2026-06-16 19:49:46 -0300

    Remove CrossOrigin annotation from ContatoController
    Removed CrossOrigin annotation for ContatoController.

--- WA10-main/backendWA10/src/main/java/com/wa10/api/controller/ContatoController.java
+++ WA10-main/backendWA10/src/main/java/com/wa10/api/controller/ContatoController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/contatos")
-@CrossOrigin(origins = "http://localhost:3000")
 public class ContatoController {
 
     @Autowired
